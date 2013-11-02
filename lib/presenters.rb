117a3af9f05f1ed55f57385cdecd4c4d4c1d5c67
Francesco Rodriguez <frodsan@me.com>
2013-11-02 00:34:06 -0500

    Returns listed attributes and their values.

--- lib/presenters.rb
+++ lib/presenters.rb
@@ -1,13 +1,27 @@
 class Presenter
   def self.attribute name
+    return if attributes.include? name
+
+    attributes << name
+
     define_method name do
       object.send name
-    end
+    end if !method_defined? name
+  end
+
+  def self.attributes
+    @attributes ||= []
   end
 
   attr :object
 
   def initialize object
     @object = object
+  end
+
+  def attributes
+    self.class.attributes.each_with_object({}) do |name, hash|
+      hash[name] = send name
+    end
   end
 end
--- test/presenters_test.rb
+++ test/presenters_test.rb
@@ -1,15 +1,29 @@
 require 'cutest'
 require_relative '../lib/presenters'
 
-Post = Struct.new :id
+Post = Struct.new :id, :name, :lastname
 
 class PostPresenter < Presenter
   attribute :id
+  attribute :fullname
+
+  def fullname
+    object.name + ' ' + object.lastname
+  end
 end
 
-test 'define method for given attribute' do
+test 'defines method for given attribute' do
   post = Post.new 1
   presenter = PostPresenter.new post
 
   assert_equal post.id, presenter.id
+end
+
+test 'returns listed attributes and their values' do
+  post = Post.new 1, 'sonny', 'moore'
+  presenter = PostPresenter.new post
+
+  result = { id: 1, fullname: 'sonny moore' }
+
+  assert_equal result, presenter.attributes
 end
