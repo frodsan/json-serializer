187541be21fddccf27890f2b8024697dbce1f731
Francesco Rodriguez <frodsan@me.com>
2013-11-02 00:14:06 -0500

    Define method for given attribute.

--- lib/presenters.rb
+++ lib/presenters.rb
@@ -1,1 +1,13 @@
+class Presenter
+  def self.attribute name
+    define_method name do
+      object.send name
+    end
+  end
 
+  attr :object
+
+  def initialize object
+    @object = object
+  end
+end
--- test/presenters_test.rb
+++ test/presenters_test.rb
@@ -1,2 +1,15 @@
 require 'cutest'
 require_relative '../lib/presenters'
+
+Post = Struct.new :id
+
+class PostPresenter < Presenter
+  attribute :id
+end
+
+test 'define method for given attribute' do
+  post = Post.new 1
+  presenter = PostPresenter.new post
+
+  assert_equal post.id, presenter.id
+end
