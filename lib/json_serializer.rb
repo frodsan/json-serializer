e7a1af9e0ee88aed605bb1b8dc3c4f94ca733feb
Francesco Rodriguez <frodsan@me.com>
2013-11-03 12:14:42 -0500

    Improve implementation.

--- lib/json_serializer.rb
+++ lib/json_serializer.rb
@@ -9,14 +9,6 @@ class JsonSerializer
     @attributes ||= []
   end
 
-  def self.root key
-    @root = key
-  end
-
-  def self.root_key
-    @root
-  end
-
   attr :object
 
   def initialize object
@@ -24,41 +16,45 @@ class JsonSerializer
   end
 
   def to_json
-    if root = self.class.root_key
-      { root => serializable_object }.to_json
+    serializable_object.to_json
+  end
+
+  def serializable_object # :nodoc
+    if object.respond_to? :to_ary
+      SerializableArray
     else
-      serializable_object.to_json
+      SerializableHash
+    end.new(object, self).serializable_object
+  end
+
+  private
+
+  class Serializable
+    attr :object, :serializer
+
+    def initialize object, context
+      @object  = object
+      @context = context
     end
   end
 
-  protected
-
-  def serializable_object
-    serializable_data
-  end
-
-  def serializable_data
-    self.class.attributes.each_with_object({}) do |name, hash|
-      hash[name] = if self.class.method_defined? name
-        send name
-      else
-        object.send name
+  class SerializableHash < Serializable
+    def serializable_object
+      context.class.attributes.each_with_object({}) do |name, hash|
+        hash[name] = if context.class.method_defined? name
+          context.send name
+        else
+          object.send name
+        end
       end
     end
   end
-end
 
-class ArrayJsonSerializer < JsonSerializer
-  def initialize object, serializer
-    @serializer = serializer
-    super object
-  end
-
-  protected
-
-  def serializable_object
-    object.to_ary.map do |item|
-      @serializer.new(item).serializable_data
+  class SerializableArray < Serializable
+    def serializable_object
+      object.to_ary.map do |item|
+        context.class.new(item).serializable_object
+      end
     end
   end
 end
--- test/json_serializer_test.rb
+++ test/json_serializer_test.rb
@@ -26,36 +26,14 @@ test 'converts defined attributes into json' do
   assert_equal result.to_json, serializer.to_json
 end
 
-class PostWithRootSerializer < JsonSerializer
-  root :post
-
-  attribute :id
-  attribute :title
-end
-
-test 'serializes object with root key' do
-  post = Post.new 1, 'tsunami'
-  serializer = PostWithRootSerializer.new post
-
-  result = { post: { id: 1, title: 'tsunami' } }.to_json
-
-  assert_equal result, serializer.to_json
-end
-
 User = Struct.new :id, :name, :lastname
 
-class UsersSerializer < ArrayJsonSerializer
-  def initialize object
-    super object, UserSerializer
-  end
+class UserSerializer < JsonSerializer
+  attribute :id
+  attribute :fullname
 
-  class UserSerializer < JsonSerializer
-    attribute :id
-    attribute :fullname
-
-    def fullname
-      object.name + ' ' + object.lastname
-    end
+  def fullname
+    object.name + ' ' + object.lastname
   end
 end
 
@@ -65,7 +43,7 @@ test 'serializes array' do
     User.new(2, 'anton', 'zaslavski')
   ]
 
-  serializer = UsersSerializer.new users
+  serializer = UserSerializer.new users
 
   result = [
     { id: 1, fullname: 'sonny moore' },
