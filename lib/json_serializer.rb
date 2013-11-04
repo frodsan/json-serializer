e74fa6df59d8b846265e1cb5c1521b5e0b093067
Francesco Rodriguez <frodsan@me.com>
2013-11-04 18:09:47 -0500

    Add documentation to #attribute and #attributes.

--- lib/json_serializer.rb
+++ lib/json_serializer.rb
@@ -1,10 +1,53 @@
 require 'json'
 
 class JsonSerializer
+  # Specify which attributes you would like to include in the outputted JSON.
+  #
+  #   class UserSerializer < JsonSerializer
+  #     attribute :id
+  #     attribute :username
+  #   end
+  #
+  #   user = User.create username: 'skrillex', admin: true
+  #
+  #   UserSerializer.new(user).to_json
+  #   # => {"id":1,"username":"skrillex"}
+  #
+  # By default, before looking up the attribute on the object, checks the presence
+  # of a method with the attribute name. This allow serializers to include properties
+  # in addition to the object attributes or customize the result of a specified
+  # attribute. You can access the object being serialized with the +object+ method.
+  #
+  #   class UserSerializer < JsonSerializer
+  #     attribute :id
+  #     attribute :username
+  #     attribute :full_name
+  #
+  #     def full_name
+  #       object.first_name + ' ' + object.last_name
+  #     end
+  #   end
+  #
+  #   user = User.create username: 'skrillex', first_name: 'sonny', last_name: 'moore'
+  #
+  #   UserSerializer.new(user).to_json
+  #   # => {"id":1,"username":"skrillex","full_name":"sonny moore"}
+  #
   def self.attribute name
     attributes << name if !attributes.include? name
   end
 
+  # Return an array with the specified attributes by +attribute+.
+  #
+  #   class UserSerializer < JsonSerializer
+  #     attribute :id
+  #     attribute :username
+  #     attribute :github
+  #   end
+  #
+  #   UserSerializer.attributes
+  #   # => [:id, :username, :github]
+  #
   def self.attributes
     @attributes ||= []
   end
