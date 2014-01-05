035e9b685c4c7211d054f40d02d5407575780a15
Francesco Rodriguez <frodsan@me.com>
2014-01-04 22:46:31 -0500

    Remove rdoc documentation.

--- lib/json-serializer.rb
+++ lib/json-serializer.rb
@@ -1,54 +1,10 @@
 require "json"
 
 class JsonSerializer
-  # Specifies which attributes you would like to include in the outputted JSON.
-  #
-  #   class UserSerializer < JsonSerializer
-  #     attribute :id
-  #     attribute :username
-  #   end
-  #
-  #   user = User.create(username: "skrillex", admin: true)
-  #
-  #   UserSerializer.new(user).to_json
-  #   # => {"id":1,"username":"skrillex"}
-  #
-  # By default, before looking up the attribute on the object, checks the presence
-  # of a method with the attribute name. This allow serializers to include properties
-  # in addition to the object attributes or customize the result of a specified
-  # attribute. You can access the object being serialized with the +object+ method.
-  #
-  #   class UserSerializer < JsonSerializer
-  #     attribute :id
-  #     attribute :username
-  #     attribute :full_name
-  #
-  #     def full_name
-  #       object.first_name + " " + object.last_name
-  #     end
-  #   end
-  #
-  #   user = User.create(username: "skrillex", first_name: "sonny", last_name: "moore")
-  #
-  #   UserSerializer.new(user).to_json
-  #   # => {"id":1,"username":"skrillex","full_name":"sonny moore"}
-  #
   def self.attribute(name, serializer = nil)
     attributes[name] ||= serializer
   end
 
-  # Returns a hash with the specified attributes as keys and their defined
-  # serializers as values.
-  #
-  #   class UserSerializer < JsonSerializer
-  #     attribute :id
-  #     attribute :username
-  #     attribute :roles, :RoleSerializer
-  #   end
-  #
-  #   UserSerializer.attributes
-  #   # => {:id=>nil, :username=>nil, :roles=>:RoleSerializer}
-  #
   def self.attributes
     @attributes ||= {}
   end
@@ -69,7 +25,7 @@ class JsonSerializer
 
   protected
 
-  def serializable_object # :nodoc:
+  def serializable_object
     if object.respond_to?(:to_ary)
       object.to_ary.map { |item| self.class.new(item).attributes }
     else
@@ -77,7 +33,7 @@ class JsonSerializer
     end
   end
 
-  def attributes # :nodoc:
+  def attributes
     self.class.attributes.each_with_object({}) do |(name, serializer), hash|
       data = self.class.method_defined?(name) ? self.send(name) : object.send(name)
       data = Utils.const(self.class, serializer).new(data).serializable_object if serializer
@@ -85,7 +41,7 @@ class JsonSerializer
     end
   end
 
-  module Utils # :nodoc
+  module Utils
     def self.const(context, name)
       case name
       when Symbol, String
